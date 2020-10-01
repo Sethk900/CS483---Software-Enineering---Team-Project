744ceec199781d9fbf15474017fc4c335e24c148
stlngds <greysonbiggs@gmail.com>
2020-09-30 17:28:31 -0700

    moved setvolume

---
+++ Fiasco_Game/Assets/src/Greyson/SetVolume.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    //public Slider slider;
+
+    /*void Start() {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        // TODO: Save and set values for ALL sliders
+    }*/
+    public void SetLevelMusic (float sliderValue) {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+
+    public void SetLevelSFX (float sliderValue) {
+        mixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetLevelMaster (float sliderValue) {
+        mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+    }
+}

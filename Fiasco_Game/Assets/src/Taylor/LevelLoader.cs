b7b7a604bb18840815126364e5cd9c3163b4df46
stew5398 <stew5398@vandals.uidaho.edu>
2020-10-07 17:32:03 -0700

    Added Level Loader Test

--- Fiasco_Game/Assets/src/Taylor/LevelLoader.cs
+++ Fiasco_Game/Assets/src/Taylor/LevelLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class LevelLoader : MonoBehaviour
 {
 	// Build indexes that map to scenes
@@ -21,10 +22,25 @@ public class LevelLoader : MonoBehaviour
 		}
     }
 	// Load Scene using it's Build index
-	public void LoadLevel(int Build_Idx) {
+	public int LoadLevel(int Build_Idx) {
+		if(Build_Idx > SceneManager.sceneCountInBuildSettings ||
+		   Build_Idx < 0) {
+			Debug.Log("Invalid Command, build inded out of range");
+			return 1;
+		}
 		StartCoroutine(LoadLevelCoroutine(Build_Idx));
+		return 0;
 	}
 	
+	public int LoadNextLevel() {
+		if(SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			return 0;
+		} else {
+			Debug.Log("Invalid Command, in last scene");
+			return 1;
+		}
+	}
 	//Coroutine for loading scene
 	IEnumerator LoadLevelCoroutine(int Build_Idx) {
 		//Play Animation
---
+++ Fiasco_Game/Assets/tst/Taylor/Tests/LvlLoaderInnerBoundary.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.TestRunner;
+
+namespace Taylor_Test
+{
+    public class LvlLoaderInnerBoundaryTest
+    {
+        [Test]
+        public void LvlLoaderInnerBoundaryTestPasses()
+        {
+			LevelLoader test = new LevelLoader();
+			
+			int expected_return = 1;
+			int return_val = test.LoadLevel(-1);
+			
+			Assert.AreEqual(expected_return, return_val);
+        }
+		
+    }
+}

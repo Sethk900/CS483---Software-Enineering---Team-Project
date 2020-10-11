1084d735e77f98b821189f2dc208998289a51fd3
stlngds <greysonbiggs@gmail.com>
2020-10-10 17:51:30 -0700

    SoundManager

---
+++ Fiasco_Game/Assets/tst/Greyson/Tests/BoundaryTest1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BoundaryTest1
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void BoundaryTest1SimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+    }
+}

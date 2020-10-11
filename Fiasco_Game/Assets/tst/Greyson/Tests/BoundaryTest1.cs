7d4b5da7e0563a1614f118ac098f63c3bd66b240
stlngds <greysonbiggs@gmail.com>
2020-10-11 13:16:55 -0700

    AudioManager tests (doesn't work ;-;)

--- Fiasco_Game/Assets/tst/Greyson/Tests/BoundaryTest1.cs
+++ Fiasco_Game/Assets/tst/Greyson/Tests/BoundaryTest1.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.Audio;
 
 namespace Tests
 {
     public class BoundaryTest1
     {
-        // A Test behaves as an ordinary method
+        public AudioMixer mixer;
+        float expectedmaxvol = 1f; //this is what the max vol is defined as in the Editor
+        float value;
         [Test]
         public void BoundaryTest1SimplePasses()
         {
-            // Use the Assert class to test conditions
+            float i;
+            for(i = 0.1f; i < 2; i += .1f) {
+                mixer.SetFloat("MasterVol", Mathf.Log10(i) * 20);
+            }
+        bool result = mixer.GetFloat("MasterVol", out float value);
+        expectedmaxvol = Mathf.Log10(expectedmaxvol) * 20;
+        Assert.AreEqual(expectedmaxvol, value); 
         }
 
     }
--- Fiasco_Game/Assets/tst/Greyson/Tests/BoundaryTest2.cs
+++ Fiasco_Game/Assets/tst/Greyson/Tests/BoundaryTest2.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.Audio;
 
 namespace Tests
 {
     public class BoundaryTest2
     {
-        // A Test behaves as an ordinary method
+        public AudioMixer mixer;
+        float expectedmaxvol = 0.0001f;
+        float value;
         [Test]
         public void BoundaryTest2SimplePasses()
         {
-            // Use the Assert class to test conditions
+            float i;
+            for(i = .9f; i > -2; i -= .1f) {
+                mixer.SetFloat("MasterVol", Mathf.Log10(i) * 20);
+            }
+        bool result = mixer.GetFloat("MasterVol", out float value);
+        expectedmaxvol = Mathf.Log10(expectedmaxvol) * 20;
+        Assert.AreEqual(expectedmaxvol, value); 
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator BoundaryTest2WithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
-        }
     }
 }

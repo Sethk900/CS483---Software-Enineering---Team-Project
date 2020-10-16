cd264b65314a62d1e8fdd90232b033e7539d90b6
Sethk900 <70236017+Sethk900@users.noreply.github.com>
2020-10-16 15:24:54 -0700

    Finalized Stress Test

--- Fiasco_Game/Assets/tst/Seth/StressTests/BreakUI.cs
+++ Fiasco_Game/Assets/tst/Seth/StressTests/BreakUI.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
 
 public class BreakUI : MonoBehaviour
 {
+	public int i = 0;
+	public int sum = 0;
+	public int IncrementValue=1; //Update IncrementValue to govern how much we increment the health and score values by
     // Start is called before the first frame update
     void Start()
     {
-        
+        UIScript.health=0;
+		UIScript.score=0;
     }
 
     // Update is called once per frame
     void Update()
     {
-      
-	  	UIScript.health+=100;
-		UIScript.score+=1000000;
-	   
+      	i++;
+	  	UIScript.health+=sum;
+		UIScript.score+=sum;
+		sum+=IncrementValue;
+		if(UIScript.health<0){
+			Debug.Log("Invalid negative value detected for health after incrementing health by "+sum+" points in"+i+" frames.");
+			SceneManager.LoadScene("DeathScreen");
+			}
+		if(UIScript.score<0){
+			Debug.Log("Invalid negative value detected for score after incrementing score by "+sum+" points in "+i+" frames.");
+	   		SceneManager.LoadScene("DeathScreen");
+				}
     }
 }
+

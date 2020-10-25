0f052b9aaea192b3f60e52d58b60687c3f9fb12f
Sethk900 <70236017+Sethk900@users.noreply.github.com>
2020-10-25 12:47:46 -0700

    Rudimentary pause screen

--- Fiasco_Game/Assets/src/Seth/Scripts/UIScript.cs
+++ Fiasco_Game/Assets/src/Seth/Scripts/UIScript.cs
@@ -10,22 +10,58 @@ public class UIScript : MonoBehaviour
     public static int score = 0;
     public static int health = 100;
     Text HUD;
+	GameObject[] pauseObjects;
 	
     // Start is called before the first frame update
     void Start()
     {
+		// Initialize the Heads Up Display
         HUD = GetComponent<Text>();
+		
+		//Initialize the objects that will be hidden until the game is paused (i.e., the objects that make up the pause menu)
+		Time.timeScale = 1;
+		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused();
     }
 
     // Update is called once per frame
     void Update()
     {
-	// Constantly update the HUD
+		// Constantly update the HUD
         HUD.text = "Score: " + score + " Health: " + health;
+
+		// Check if the player paused the game
+		if(Input.GetKeyDown(KeyCode.P)){
+		if(Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+			} else if (Time.timeScale == 0){
+				Debug.Log ("high");
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
 
 		// Later we will implement logic here to load the death screen
 		if(health==0){
 			HUD.text = "You died :(";
 		}
     }
+	
+	void hidePaused()
+ {
+     foreach (GameObject p in pauseObjects)
+     {
+         p.SetActive(false);
+     }
+ }
+ 
+ 	void showPaused()
+ {
+     foreach (GameObject p in pauseObjects)
+     {
+         p.SetActive(true);
+     }
+ }
 }

540f101eb05051e983e1ebd9c64f1755917e9546
MaxI <maxwellicardo@gmail.com>
2020-11-04 14:57:06 -0800

    Reworked BulletPool script

---
+++ Fiasco_Game/Assets/src/Max/BulletManage/Scripts/attempt 2/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     //   transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+}

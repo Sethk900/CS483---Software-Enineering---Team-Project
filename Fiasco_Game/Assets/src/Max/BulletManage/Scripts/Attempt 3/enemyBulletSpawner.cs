34d67b40b2c47e1dc8e2a8f4c092f5559844afb6
MaxI <maxwellicardo@gmail.com>
2020-11-07 13:48:14 -0800

    Added a couple scripts

---
+++ Fiasco_Game/Assets/src/Max/BulletManage/Scripts/Attempt 3/enemyBulletSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBulletSpawner : MonoBehaviour
+{
+
+    objectPooler objectPooler;
+
+    private void Start(){
+        objectPooler = objectPooler.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectPooler.SpawnFromPool("EnemyBullet", transform.position, Quaternion.identity);
+    }
+}

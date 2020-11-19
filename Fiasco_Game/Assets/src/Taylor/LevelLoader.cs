9afef49d082cda64430208f6a1eea5c6b0eeb65e
stlngds <greysonbiggs@gmail.com>
2020-11-18 20:51:11 -0800

    Revert "Improved Documentation"
    This reverts commit 2a21fb06024394f0cbd334128b48526d5ce97d4c.

--- Fiasco_Game/Assets/src/Taylor/LevelLoader.cs
+++ Fiasco_Game/Assets/src/Taylor/LevelLoader.cs
@@ -3,31 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public sealed class LevelLoader : MonoBehaviour
+public class LevelLoader : MonoBehaviour
 {	
-	// Thread safe and lazy singleton implementation
-	private LevelLoader() {}
 	
 	public Animator transition;
 	public float transitionTime = 1f;
 	
-	/*
-	 *	Used to access the LevelLoader singleton.
-	 *  Public methods can be called from other
-	 *  scripts using this method in the following format
-	 *  
-     *  LevelLoader.Instance.LoadLevel(<<build index>>);
-	 */	
+	// Thread safe and lazy singleton implementation
+	private LevelLoader() {}
 	public static LevelLoader Instance {
 		get {
 			return Nested.instance;
 		}
 	}
 	
-	/* 
-	 *  Public function used to load a level
-	 *  along with a crossfade animation
-	 */
+	private class Nested {
+		// Explicit static constructor to tell C# compiler
+		// not to mark type as beforefieldinit
+		static Nested() {}
+		internal static readonly LevelLoader instance = new LevelLoader();
+	}
+	
+	// Load Scene using it's Build index
 	public int LoadLevel(int Build_Idx) {
 		if(SceneManager.sceneCountInBuildSettings < Build_Idx || 0 > Build_Idx) { 
 			Debug.Log("Invalid Build_Idx");
@@ -37,12 +34,10 @@ public sealed class LevelLoader : MonoBehaviour
 		return 0;
 	}
 	
-	/*
-	 * Coroutine used to play level animation,
-	 * wait for transition time, and then load
-	 * a new level
-	 */
-	private IEnumerator LoadLevelCoroutine(int Build_Idx) {
+	//Coroutine for loading scene with animation
+	IEnumerator LoadLevelCoroutine(int Build_Idx) {
+		//Play success jingle
+		FindObjectOfType<AudioManager>().Play("Success"); // - Greyson
 		//Play Animation
 		transition.SetTrigger("Start");
 	
@@ -51,15 +46,5 @@ public sealed class LevelLoader : MonoBehaviour
 	
 		//Load Scene
 		SceneManager.LoadScene(Build_Idx);
-	}
-	
-	/*
-	 * Private class used to implement 
-	 * LevelLoader as a singleton
-	 */
-	private class Nested {
-		// internal allows Nested to access private constructor
-		static Nested() {}
-		internal static readonly LevelLoader instance = new LevelLoader();
 	}
 }
---
+++ Fiasco_Game/Assets/tst/Taylor/LevelLoaderInnerBoundary.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.TestRunner;
+
+namespace LevelLoaderTest
+{
+    public class LevelLoaderInnerBoundry
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void LevelLoaderInnerBoundryPasses()
+        {
+			int return_val = LevelLoader.Instance.LoadLevel(-1);
+			int expected_return = 1;
+			
+			Assert.AreEqual(expected_return, return_val);
+        }
+    }
+}

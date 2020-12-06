7a03220fd99403fae971f85ee0cf36da150b9ed3
stlngds <64813108+stlngds@users.noreply.github.com>
2020-12-05 21:04:30 -0900

    Inventory framework, test push
    pain

---
+++ Fiasco_Game/Assets/src/Greyson/Item.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Item : MonoBehaviour, IPointerClickHandler
+{
+    public ItemTypes Type;
+
+    public Inventory Inventory;
+
+    public void OnPointerClick(PointerEventData eventData) {
+        if (eventData.button == PointerEventData.InputButton.Left) {
+            Inventory.Add(Type);
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right) {
+            Debug.Log(message: Inventory.Get(Type));
+        }
+    }
+}
